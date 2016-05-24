a9f087371121ff729dafd217cc2122cc0548e4e8
Paul <pacalabre@gmail.com>
2016-05-24 15:32:47 -0700

    added API request

--- starter-code/js/app.js
+++ starter-code/js/app.js
@@ -1,3 +1,17 @@
-/* put js code here */
+var reddItApp = angular.module('reddItApp',[]);
+reddItApp.controller( 'reddItCtrl',['$scope', '$http', function($scope, $http){
 
-console.log('this file is running.');
+  $scope.search = function(){
+    var req = {
+      url:'http://www.reddit.com/search.json?q='+ $scope.searchTerm,
+      method:'GET'
+    }
+
+    $http(req).then(function success(res) {
+      var redditData = res.data.data.children;
+      console.log(res.data.data.children);
+    }, function error(error) {
+      console.log(error);
+    });
+  }
+}]);
