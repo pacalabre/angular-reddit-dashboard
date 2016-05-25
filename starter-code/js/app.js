ac1d8ef92ff4586394d09e65240203f4cbd8199d
Paul <pacalabre@gmail.com>
2016-05-24 17:27:48 -0700

    can add search to history

--- starter-code/js/app.js
+++ starter-code/js/app.js
@@ -2,9 +2,8 @@ var reddItApp = angular.module('reddItApp',[]);
 
 reddItApp.controller( 'reddItCtrl',['$scope', '$http', function($scope, $http){
   $scope.searchTerm = '';
-  $scope.postInfo=[]
-
-
+  $scope.postInfo=[];
+  $scope.history=[];
 
   $scope.search = function(){
     var req = {
@@ -13,20 +12,14 @@ reddItApp.controller( 'reddItCtrl',['$scope', '$http', function($scope, $http){
     }
 
     $http(req).then(function success(res) {
+
+      $scope.history.push($scope.searchTerm);
+      console.log("history = "+$scope.history);
       var redditData = res.data.data.children;
-      console.log(redditData.length);
       for(var i=0;i<redditData.length;i++){
         $scope.postInfo.push(redditData[i].data)
-        // $scope.postInfo.title = redditData[i].data.title;
-        // $scope.postInfo.author = redditData[i].data.author;
-        // $scope.postInfo.ups = redditData[i].data.ups;
-        // $scope.postInfo.comments = redditData[i].data.num_comments;
-        // $scope.postInfo.image = redditData[i].data.thumbnail;
-        // console.log($scope.postInfo.ups);
         console.log($scope.postInfo)
       }
-      console.log($scope.title);
-      console.log(res.data.data.children);
     }, function error(error) {
       console.log(error);
     });
